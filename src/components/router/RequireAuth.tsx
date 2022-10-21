e5482f634a0bfba9e2e0c4c2e6de0242939c49b3
Luke <luke.anhnguyen@setel.com>
2022-10-22 00:25:48 +0700

    update role route

--- src/components/router/RequireAuth.tsx
+++ src/components/router/RequireAuth.tsx
@@ -2,12 +2,13 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useAppSelector } from '@app/hooks/reduxHooks';
 import Error404Page from '@app/pages/Error404Page';
 
-const RequireAuth = ({ children, roles }: { children: JSX.Element; roles: Array<any> }) => {
+const RequireAuth = ({ children, roles }: { children: JSX.Element; roles?: Array<any> }) => {
   const location = useLocation();
   const token = useAppSelector((state) => state.auth.token);
   const user = useAppSelector((state) => state.user.user);
 
-  const userHasRequiredRole = token && user && roles.includes(user?.role) && roles.length > 0 ? true : false;
+  const userHasRequiredRole =
+    (token && user && roles?.includes(user?.role) && roles?.length > 0) || !roles ? true : false;
 
   if (!token) {
     return <Navigate to={`auth/login`} state={{ from: location }} />;
